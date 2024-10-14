db86922079bc769dbd6cdaacdf2bb6e659206b2c
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-14 18:15:26 +0200

    Change test code

--- Program.cs
+++ Program.cs
@@ -2,26 +2,14 @@
 //Mauricio Corte
 //.NET24
 
+using Spectre.Console;
 namespace MyFirstProgram
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int number = 11; //int variable "number" with the value 11.
-
-            if (number > 10) { // if-statement that checks if "number" is larger than 10.
-                Console.WriteLine("Talet är stort!");
-            }
-
-            Console.WriteLine("Vad heter du?"); //Promots the user for input.
-            string? name = Console.ReadLine(); //Declaring the string variable "name" and letting the user input the value. We have a "?" after the type beacuse the value can be null.
-
-            Console.WriteLine($"Hej {name}!"); //Printing out the users name with string interpolation using "$".
-
-            for (int i = 0; i <= number; i++) { //A for-loop that prints out the value of "i" until it is equal to the value of "number".
-                Console.WriteLine(i);
-            }
+            Menu.MainMenu();
         }
     }
 }
