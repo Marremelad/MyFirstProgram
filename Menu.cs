a2f25353bf399bafd258a66851912fb31c027247
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-14 17:57:29 +0200

    Use standard enums as menu choice

--- Menu.cs
+++ Menu.cs
@@ -6,25 +6,37 @@ public class Menu
 {
     public static void MainMenu()
     {
-        var choice = AnsiConsole.Prompt(new SelectionPrompt<string>()
+        
+        var choice = AnsiConsole.Prompt(new SelectionPrompt<MenuChoice>()
             .Title("Welcome to my first program!!!")
             .PageSize(10)
             .AddChoices(
-                MenuChoice.Number.GetDescription(),
-                MenuChoice.Greet.GetDescription(),
-                MenuChoice.Count.GetDescription()));
-
+                MenuChoice.Number,
+                MenuChoice.Greet,
+                MenuChoice.Count));
+        
         switch (choice)
         {
-            case "Pick a number":
-                throw new NotImplementedException();
+            case MenuChoice.Number:
+                Console.WriteLine("Number");
                 break;
-            case "Tell me your name":
-                throw new NotImplementedException();
+            case MenuChoice.Greet:
+                Console.WriteLine("Greet");
                 break;
-            case "Count":
-                throw new NotImplementedException();
+            case MenuChoice.Count:
+                Console.WriteLine("Count");
                 break;
         }
+    }
+
+    public static void ChoiceNumber()
+    {
+        var number = AnsiConsole.Ask<int>("Pick a number!");
+        Helper.HigherThanTen(number);
+    }
+
+    public static void ChoiceGreet()
+    {
+        Helper.Greet();
     }
 }
