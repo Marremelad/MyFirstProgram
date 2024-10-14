c5bbe7d36d57b6571fc456dce9385448f8f6039b
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-14 16:38:41 +0200

    Added methods to Helper class

--- Helper.cs
+++ Helper.cs
@@ -1,6 +1,20 @@
 namespace MyFirstProgram;
 
-public class Helper
+public abstract class Helper
 {
-    
+    public static void HigherThanTen(int number)
+    {
+        Console.WriteLine(number > 10 ? "Your number is higher than 10!" : "Your number is not higher than 10!");
+    }
+
+    public static void Greet()
+    {
+        string? name;
+        while (string.IsNullOrEmpty(name = Console.ReadLine()))
+        {
+            Console.WriteLine("Your name can not be empty!");
+        }
+        
+        Console.WriteLine($"Hello {name}!");
+    }
 }
