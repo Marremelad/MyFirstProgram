c5a26acfccf1b72412f644c5a7128daabd5befdf
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-14 17:22:14 +0200

    Changed menu options from strings to enums

--- Menu.cs
+++ Menu.cs
@@ -1,6 +1,30 @@
+using Spectre.Console;
+
 namespace MyFirstProgram;
 
 public class Menu
 {
-    
+    public static void MainMenu()
+    {
+        var choice = AnsiConsole.Prompt(new SelectionPrompt<string>()
+            .Title("Welcome to my first program!!!")
+            .PageSize(10)
+            .AddChoices(
+                MenuChoice.Number.GetDescription(),
+                MenuChoice.Greet.GetDescription(),
+                MenuChoice.Count.GetDescription()));
+
+        switch (choice)
+        {
+            case "Pick a number":
+                throw new NotImplementedException();
+                break;
+            case "Tell me your name":
+                throw new NotImplementedException();
+                break;
+            case "Count":
+                throw new NotImplementedException();
+                break;
+        }
+    }
 }
